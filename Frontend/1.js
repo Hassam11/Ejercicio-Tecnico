693c248e698c2f98907cd3ff855132e918f35dbb
Hassamch11 <92774697+Hassam11@users.noreply.github.com>
2022-09-04 15:44:33 -0500

    2

--- Frontend/1.js
+++ Frontend/1.js
@@ -1,34 +1,34 @@
-const Api = async() => {
-    try{
-        const res = await fetch(`http://127.0.0.1:5000/productos`)
-        const data = await res.json() 
+const items = document.getElementById("items");
+const cardItem = document.getElementById("cardItem");
 
-        let nombre = ''
-        let imagenes = ''
-        data.Productos.forEach(element => {
-            nombre = nombre + `<h4>${element.name}</h4>`
-            imagenes = imagenes + `<img src=${element.url_image} alt=${element.url_image}  width="400px" height="400px" />`
+const fetchData = async () => {
+  try {
+    const res = await fetch(`http://127.0.0.1:5000/productos`);
+    const data = await res.json();
 
-            document.getElementById('card').innerHTML = nombre
-            document.getElementById('card').innerHTML = imagenes
-            /*console.log(element.name)*/
-        });
-        
-    
+    data.Productos.forEach((element) => {
+      const div = document.createElement("div");
+      div.classList.add("card");
 
-        /*
-        const prod = info.map((produc) => {
-            return `<p>nombre: ${produc[2].name}</p>
-            <p>id: ${produc[2].id}</p>
-            <p>categoria: ${produc[2].category}</p>
-            <img src=${produc[2].url_image} alt=${produc[2].name}/>
-            <p>Precio: ${produc[2].price}</p>
-            `
-        })
-        root.innerHTML = prod
-        console.log(prod)*/
-    }catch(error){
-        console.log(error)
-    }  
-}
-Api()
+      div.style.width = "24rem";
+      div.style.borderRadius = ".8rem";
+      div.innerHTML = `
+                <div class="card-body " >
+                <img  class="card-img-top img-productos" src=${element.url_image} alt=${element.url_image}  width="346px" height="465px" />
+                    <h3 class="card-title text-center">${element.name}</h3>
+                    <div class="precio">
+                        <span class="fs-3 text-danger" class="card-text">$${element.price.toFixed(2)}</span>
+                        <button class="btn btn-dark col-9 mx-auto fs-3">Comprar</button>
+                    </div>
+                </div>
+            `;
+      items.appendChild(div);
+    });
+
+    //<p>${element.discount}</p>
+    //${element.category}</p>
+  } catch (error) {
+    console.log(error);
+  }
+};
+fetchData();
