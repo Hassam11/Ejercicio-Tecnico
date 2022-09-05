933072ea4784af9d45e20f3e438d6b7eed2872d0
Hassamch11 <92774697+Hassam11@users.noreply.github.com>
2022-09-04 23:28:02 -0500

    Final

--- Frontend/1.js
+++ Frontend/1.js
@@ -1,34 +1,44 @@
 const items = document.getElementById("items");
 const cardItem = document.getElementById("cardItem");
+const input = document.querySelector("#searchInput");
 
 const fetchData = async () => {
   try {
     const res = await fetch(`http://127.0.0.1:5000/productos`);
     const data = await res.json();
-
-    data.Productos.forEach((element) => {
-      const div = document.createElement("div");
-      div.classList.add("card");
-
-      div.style.width = "24rem";
-      div.style.borderRadius = ".8rem";
-      div.innerHTML = `
-                <div class="card-body " >
-                <img  class="card-img-top img-productos" src=${element.url_image} alt=${element.url_image}  width="346px" height="465px" />
-                    <h3 class="card-title text-center">${element.name}</h3>
-                    <div class="precio">
-                        <span class="fs-3 text-danger" class="card-text">$${element.price.toFixed(2)}</span>
-                        <button class="btn btn-dark col-9 mx-auto fs-3">Comprar</button>
-                    </div>
-                </div>
-            `;
-      items.appendChild(div);
-    });
-
-    //<p>${element.discount}</p>
-    //${element.category}</p>
-  } catch (error) {
+  
+    
+        data.Productos.forEach((element) => {
+            const div = document.createElement("div");
+            div.classList.add("card");
+            div.style.width = "24rem";
+            div.style.borderRadius = ".8rem";
+      
+            div.innerHTML = `
+                      <div class="card-body" data-categorias=${
+                        element.category.id
+                      } data-nombre=${element.name}>
+                          <img  class="card-img-top img-productos" src=${
+                            element.url_image
+                          } alt=${element.url_image}  width="346px" height="465px" />
+                          <h3 class="card-title text-center">${element.name}</h3>
+                          <div class="precio">
+                              <span class="fs-3 text-danger" class="card-text">$${element.price.toFixed(
+                                2
+                              )}</span>
+                              <button class="btn btn-dark col-9 mx-auto fs-3">Comprar</button>
+                          </div>
+                      </div>
+                  `;
+        items.appendChild(div);
+          });
+    
+    
+  }catch (error) {
     console.log(error);
   }
 };
 fetchData();
+
+
+
